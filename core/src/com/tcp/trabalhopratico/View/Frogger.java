5d0584ac87d356006337c3c62134adc281501159
JefersonFG <jefersonfguimaraes@gmail.com>
2017-06-25 15:26:08 -0300

    Ajustada classe Frogger
    Documentação e remoção de código desnecessário.

--- core/src/com/tcp/trabalhopratico/View/Frogger.java
+++ core/src/com/tcp/trabalhopratico/View/Frogger.java
@@ -2,20 +2,24 @@ package com.tcp.trabalhopratico.View;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.Game;
+import com.tcp.trabalhopratico.Helper.Assets;
+import com.tcp.trabalhopratico.Helper.Settings;
 
+/**
+ * Classe que inicia o software. Responsável por carregar os recursos e iniciar
+ * a tela principal.
+ */
 public class Frogger extends Game {
-	public SpriteBatch batcher;
-	
-	@Override
-	public void create () {
-		batcher = new SpriteBatch();
-		//Settings.load();
-		com.tcp.trabalhopratico.Helper.Assets.load();
-		setScreen(new MainMenuScreen(this));
-	}
+	SpriteBatch batcher;
 
+	/**
+	 * Carrega a pontuação e as imagens do jogo e inicia a tela principal.
+	 */
 	@Override
-	public void render() {
-		super.render();
+	public void create() {
+		batcher = new SpriteBatch();
+		Settings.load();
+		Assets.load();
+		setScreen(new MainMenuScreen(this));
 	}
 }
