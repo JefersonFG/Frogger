e2ea8b5a0125ca25ec27b9346d6a6f3bb314e35a
JefersonFG <jefersonfguimaraes@gmail.com>
2017-06-28 22:00:14 -0300

    Adicionada classe Obstacle e atualizada documentação

---
+++ core/src/com/tcp/trabalhopratico/model/Obstacle.java
@@ -0,0 +1,18 @@
+package com.tcp.trabalhopratico.model;
+
+/**
+ * Superclasse da hierarquia de obstáculos. Obstáculos impedem que o sapo ocupe a mesma posição
+ * que o obstáculo mas não causam danos.
+ */
+public class Obstacle extends GameObject {
+    /**
+     * Implementação básica do construtor que chama o da superclasse
+     * @param x Posição do objeto no eixo x.
+     * @param y Posição do objeto no eixo y.
+     * @param width Largura do objeto.
+     * @param height Altura do objeto.
+     */
+    public Obstacle (float x, float y, float width, float height) {
+        super(x, y, width, height);
+    }
+}
