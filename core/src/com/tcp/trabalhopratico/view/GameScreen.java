5431c5f44ee0c53ad298e713c8128783cfb773c1
JefersonFG <jefersonfguimaraes@gmail.com>
2017-06-30 23:29:39 -0300

    Atualizada GameScreen para exibir vidas e tempo restante e atualizada documentação

--- core/src/com/tcp/trabalhopratico/view/GameScreen.java
+++ core/src/com/tcp/trabalhopratico/view/GameScreen.java
@@ -7,8 +7,15 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.tcp.trabalhopratico.controller.World;
+import com.tcp.trabalhopratico.controller.WorldRenderer;
 import com.tcp.trabalhopratico.helper.Assets;
 
+/**
+ * Classe que representa a tela de jogo. Contém instâncias do Controller e exibe ao usuário
+ * informações de tempo e vida restantes no jogo. É responsável por pausar o jogo e exibir a tela
+ * de fim de jogo.
+ */
 class GameScreen extends ScreenAdapter {
     private static final int GAME_READY = 0;
     private static final int GAME_RUNNING = 1;
@@ -20,31 +27,45 @@ class GameScreen extends ScreenAdapter {
     private int state;
     private OrthographicCamera guiCam;
     private Vector3 touchPoint;
-    //World world;
-    //WorldRenderer renderer;
+    private World world;
+    private WorldRenderer renderer;
     private Rectangle pauseBounds;
     private Rectangle resumeBounds;
     private Rectangle quitBounds;
     private int lastScore;
+    private int lastLives;
+    private int lastTime;
     private String scoreString;
+    private String livesString;
+    private String timeString;
 
     private GlyphLayout glyphLayout = new GlyphLayout();
 
-    public GameScreen (Frogger game) {
+    /**
+     * Construtor que inicia o jogo, instanciando o controller
+     * @param game Referência ao objeto de jogo para renderização de elementos na tela.
+     */
+    GameScreen (Frogger game) {
         this.game = game;
 
         state = GAME_READY;
         guiCam = new OrthographicCamera(320, 480);
         guiCam.position.set(320 / 2, 480 / 2, 0);
         touchPoint = new Vector3();
-        //world = new World(worldListener);
-        //renderer = new WorldRenderer(game.batcher, world);
+        world = new World();
+        renderer = new WorldRenderer(game.batcher, world);
         pauseBounds = new Rectangle(320 - 64, 480 - 64, 64, 64);
         resumeBounds = new Rectangle(160 - 96, 240, 192, 36);
         quitBounds = new Rectangle(160 - 96, 240 - 36, 192, 36);
         scoreString = "SCORE: 0";
+        livesString = "LIVES: 0";
+        timeString = "TIME: 0";
     }
 
+    /**
+     * Atualiza o conteúdo na tela com o passar do tempo.
+     * @param deltaTime Tempo em segundos desde a última atualização.
+     */
     private void update (float deltaTime) {
         if (deltaTime > 0.1f) deltaTime = 0.1f;
 
@@ -64,12 +85,21 @@ class GameScreen extends ScreenAdapter {
         }
     }
 
+    /**
+     * Atualiza o jogo durante o estado READY. Quando o usuário clica na tela o
+     * estado é alterado para RUNNING.
+     */
     private void updateReady () {
         if (Gdx.input.justTouched()) {
             state = GAME_RUNNING;
         }
     }
 
+    /**
+     * Atualiza o jogo no estado RUNNING. É realizada uma chamada para o método de atualização
+     * do controller e verificado o estado resultante do jogo.
+     * @param deltaTime Tempo em segundos desde a última atualização.
+     */
     private void updateRunning (float deltaTime) {
         if (Gdx.input.justTouched()) {
             guiCam.unproject(touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0));
@@ -113,6 +143,10 @@ class GameScreen extends ScreenAdapter {
         */
     }
 
+    /**
+     * Atualiza o jogo no estado PAUSED. Verifica se o usuário clicou no botão de resumir o jogo
+     * ou de finalizar a partida e retornar ao menu principal.
+     */
     private void updatePaused () {
         if (Gdx.input.justTouched()) {
             guiCam.unproject(touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0));
@@ -125,17 +159,25 @@ class GameScreen extends ScreenAdapter {
         }
     }
 
+    /**
+     * Atualiza o jogo no estado GAMEOVER. Quando o usuário clica na tela ele é levado de volta
+     * ao menu principal.
+     */
     private void updateGameOver () {
         if (Gdx.input.justTouched()) {
             game.setScreen(new MainMenuScreen(game));
         }
     }
 
-    public void draw () {
+    /**
+     * Atualiza as imagens na tela, chamando o método do controller e depois o método de atualização
+     * apropriado para o estado atual da tela.
+     */
+    private void draw () {
         GL20 gl = Gdx.gl;
         gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        //renderer.render();
+        renderer.render();
 
         guiCam.update();
         game.batcher.setProjectionMatrix(guiCam.combined);
@@ -158,32 +200,54 @@ class GameScreen extends ScreenAdapter {
         game.batcher.end();
     }
 
+    /**
+     * Atualiza o conteúdo na tela no estado READY, que é a palavra READY sobre o fundo do jogo.
+     */
     private void presentReady () {
         game.batcher.draw(Assets.ready, 160 - 192 / 2, 240 - 32 / 2, 192, 32);
     }
 
+    /**
+     * Atualiza o conteúdo na tela no estado RUNNING, que são os indicadores de vidas e tempo restantes.
+     */
     private void presentRunning () {
-        game.batcher.draw(Assets.pause, 320 - 64, 480 - 64, 64, 64);
-        Assets.font.draw(game.batcher, scoreString, 16, 480 - 20);
+        game.batcher.draw(Assets.pause, 320 - 55, 480 - 55, 55, 55);
+        Assets.font.draw(game.batcher, livesString, 10, 480 - 10);
+        Assets.font.draw(game.batcher, timeString, 10, 480 - 30);
     }
 
+    /**
+     * Atualiza o conteúdo na tela no estado PAUSED, que são as opções de voltar ao jogo, terminar o jogo
+     * e os indicadores de vidas e tempo restantes.
+     */
     private void presentPaused () {
         game.batcher.draw(Assets.pauseMenu, 160 - 192 / 2, 240 - 96 / 2, 192, 96);
-        Assets.font.draw(game.batcher, scoreString, 16, 480 - 20);
+        Assets.font.draw(game.batcher, livesString, 10, 480 - 10);
+        Assets.font.draw(game.batcher, timeString, 10, 480 - 30);
     }
 
+    /**
+     * Atualiza o conteúdo na tela no estado GAMEOVER, que é o texto GAMEOVER e a pontuação final.
+     */
     private void presentGameOver () {
         game.batcher.draw(Assets.gameOver, 160 - 160 / 2, 240 - 96 / 2, 160, 96);
         glyphLayout.setText(Assets.font, scoreString);
         Assets.font.draw(game.batcher, scoreString, 160 - glyphLayout.width / 2, 480 - 20);
     }
 
+    /**
+     * Atualiza o conteúdo na tela.
+     * @param delta Tempo em segundos desde a última atualização.
+     */
     @Override
     public void render (float delta) {
         update(delta);
         draw();
     }
 
+    /**
+     * Pausa o jogo quando o software recebe um sinal de pause global.
+     */
     @Override
     public void pause () {
         if (state == GAME_RUNNING)
